cd43e35c67b6319b14a1cd75e71b1dd3d3353021
José C B Junior <142643035+ZehHey@users.noreply.github.com>
2024-03-05 22:14:20 -0300

    comando insert

--- Aula08/db.js
+++ Aula08/db.js
@@ -3,7 +3,6 @@ const conectar = async () => {
         return global.conexao
     const mysql = require('mysql2/promise')
     const con = mysql.createConnection('mysql://root:Guitarra13121990@localhost:3306/cfbcursos')
-    console.log('Conectado ao banco')
     global.conexao = con
     return con
 }
@@ -13,4 +12,12 @@ const todosClientes = async()=>{
     const [linhas] = await con.query('SELECT * FROM clientes_node')
     return await linhas
 }
-module.exports ={todosClientes}
+
+const insereCliente = async(cliente)=>{
+    const con =await conectar()
+    const sql = 'INSERT INTO clientes_node (s_nome, i_idade) VALUES (?, ?)'
+    const valores=[cliente.s_nome, cliente.i_idade]
+    await con.query(sql,valores)
+    
+}
+module.exports ={todosClientes, insereCliente}
--- Aula08/index.js
+++ Aula08/index.js
@@ -1,5 +1,8 @@
 (async () =>{
     const db = require('./db')
+    console.log('Inserindo novo cliente')
+    await db.insereCliente({s_nome: 'João', i_idade: 14})
+
     console.log('Obter todos os clientes')
     const clientes = await db.todosClientes()
     console.log(clientes)
