9414d9b587894a3a19431cbd5ffeaa285902d5d4
José C B Junior <142643035+ZehHey@users.noreply.github.com>
2024-03-13 21:41:09 -0300

    aula13 pt2 created

---
+++ Aula13/pt2/index.js
@@ -0,0 +1,18 @@
+const {MongoClient} = require('mongodb')
+const uri = 'mongodb+srv://zehheydev:5WBbbWx0ZolWDWRi@cluster0.iaiv9cl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+const client = new MongoClient(uri)
+const colecao = 'cursos'
+async function run(){
+    try{
+        await client.connect()
+        console.log('conectado ao banco de dados')
+
+        const dbo = client.db('cfbcursos')
+        const res = await dbo.collection(colecao).find({}).toArray()
+        console.log(res.curso)
+    }finally{
+        await client.close()
+    }
+}
+run().catch(console.dir)
